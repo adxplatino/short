bc568d749be50e1aa476e68c4df43200d0cd0e54
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-09-26 05:20:07 -0600

    1
    1

--- link/script.js
+++ link/script.js
@@ -150,12 +150,6 @@ if (keyword.includes(extraida)) {
     if (sessionStorage.getItem("setPagina")) {
         var getPagina = sessionStorage.getItem("setPagina");
         document.getElementById("LINK").innerHTML = '<section class="content-container" style="height: auto;"><div class="card"><div class="card-body text-center"><p><b>Wait a moment please...</b></p><center><div id="adsContenedor"></div></center><button btn-captcha="" class="btn btn-primary" id="enlacecaptcha" m-2="" style="width: 300px;" disabled="disabled"><b>Please wait ... <span id="timer">10</span></b></button></br></br><center><div id="adsContenedor2"></div></center></div></div></section>';
-        if (sessionStorage.getItem("setPagina") === "?manual-electricidad-basica") {
-         document.getElementById("titulot").innerHTML = '<b>MANUAL DE ELECTRICIDAD BASICA PDF</b>';
-        }else{
-        console.log("No hay titulo");
-        }
-        
         sessionStorage.clear(setTimeout(1000));
         getTiempoCaptcha('Click here to continue');
         setAdsUp();
