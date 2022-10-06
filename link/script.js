ba81144c641fcc709338c19c55bbb6397c76e25e
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2022-10-05 23:23:55 -0600

    1
    1

--- link/script.js
+++ link/script.js
@@ -151,7 +151,7 @@ if (keyword.includes(extraida)) {
 
     if (sessionStorage.getItem("setPagina")) {
         var getPagina = sessionStorage.getItem("setPagina");
-        document.getElementById("LINK").innerHTML = '<section class="content-container" style="height: auto;"><div class="card-body text-center"><p><b>Wait a moment please...</b></p><center><div id="adsContenedor"></div></center><h2 text-center id="titulo"></h2><button btn-captcha="" class="btn btn-primary" id="enlacecaptcha botonLink" m-2="" style="width: 300px;" disabled="disabled"><b>Please wait ... <span id="timer">20</span></b></button></br></br><center><div id="adsContenedor2"></div></center></div></section>';
+        document.getElementById("LINK").innerHTML = '<div class="card-body text-center"><p><b>Wait a moment please...</b></p><center><div id="adsContenedor"></div></center><h2 text-center id="titulo"></h2><button btn-captcha="" class="btn btn-primary" id="enlacecaptcha botonLink" m-2="" style="width: 300px;" disabled="disabled"><b>Please wait ... <span id="timer">20</span></b></button></div>';
         sessionStorage.clear(setTimeout(1000));
         getTiempoCaptcha('Click here to continue');
         setAdsUp();
