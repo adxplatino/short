97881956206207840b7fae948c223be5c3237163
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2023-01-13 22:04:13 -0600

    1
    1

--- download/script.js
+++ download/script.js
@@ -1,7 +1,12 @@
- var keyword = [
-  "?coleccion-de-circuitos-de-electronica",
-  "?manual-de-electronica-basica-en-pdf",
-  "?manual-de-circuitos-electricos-en-pdf"
+var keyword = [
+"?coleccion-de-circuitos-de-electronica",
+"?manual-de-electronica-basica-en-pdf",
+"?manual-de-circuitos-electricos-en-pdf",
+"?como-construir-tu-propio-ordenador",
+"?como-construir-tu-propio-ordenador",
+"?manual-definitivo-de-soldadura",
+"?manual-de-reparacion-de-lavadoras-en-pdf",
+"?manual-de-reparacion-de-refrigeradores"
 ]
 var randomUrl = ["/p/download-file.html",
   "/p/download-file.html"
@@ -96,7 +101,106 @@ if (keyword.includes(extraida)) {
             document.getElementById("linkDownload").style.display = "inline";
           }
         }
-      }
+      } else if (sessionStorage.getItem("setPagina") === "?como-construir-tu-propio-ordenador") {
+      document.getElementById("donwload-title").innerHTML = "Descarga Aqui El Manual de Cómo construir tu propio ordenador desde cero: una guía para principiantes Totalmente Gratis";
+      document.getElementById("downtitle").innerHTML = "Cómo construir tu propio ordenador desde cero: una guía para principiantes";
+      document.getElementById("downtype").innerHTML = "PDF";
+      document.getElementById("downloads").innerHTML = "9715";
+      document.getElementById("downsize").innerHTML = "(6 MB)";
+      document.getElementById("LINK").setAttribute("href", "https://drive.google.com/file/d/1EvjzoTooiYtMt3r9JqBDUEqN8TlPR50j");
+
+      function getLink() {
+        sessionStorage.clear(setTimeout(1000));
+        document.getElementById("contador").style.display = "inline";
+        document.getElementById("downboton").style.display = "none";
+        let time_out = 1000;
+        let counter_id;
+        let seconds = 15;
+        counter_id = setInterval(update_counter_link, time_out);
+        function update_counter_link() {
+          document.getElementById('link_button').innerHTML = (--seconds);
+          if (!seconds) {
+            clearInterval(counter_id);
+            document.getElementById("contador").style.display = "none";
+            document.getElementById("linkDownload").style.display = "inline";
+          }
+        }
+       
+       } else if (sessionStorage.getItem("setPagina") === "?manual-definitivo-de-soldadura") {
+      document.getElementById("donwload-title").innerHTML = "Descarga Aqui El Manual Definitivo de Soldadura Electrónica Totalmente Gratis";
+      document.getElementById("downtitle").innerHTML = "Manual Definitivo de Soldadura Electrónica";
+      document.getElementById("downtype").innerHTML = "PDF";
+      document.getElementById("downloads").innerHTML = "1142 ";
+      document.getElementById("downsize").innerHTML = "(47 MB)";
+      document.getElementById("LINK").setAttribute("href", "https://drive.google.com/file/d/1EvjzoTooiYtMt3r9JqBDUEqN8TlPR50j");
+
+      function getLink() {
+        sessionStorage.clear(setTimeout(1000));
+        document.getElementById("contador").style.display = "inline";
+        document.getElementById("downboton").style.display = "none";
+        let time_out = 1000;
+        let counter_id;
+        let seconds = 15;
+        counter_id = setInterval(update_counter_link, time_out);
+        function update_counter_link() {
+          document.getElementById('link_button').innerHTML = (--seconds);
+          if (!seconds) {
+            clearInterval(counter_id);
+            document.getElementById("contador").style.display = "none";
+            document.getElementById("linkDownload").style.display = "inline";
+          }
+        }
+       
+       } else if (sessionStorage.getItem("setPagina") === "?manual-de-reparacion-de-lavadoras-en-pdf") {
+      document.getElementById("donwload-title").innerHTML = "Descarga Aqui El Manual de reparación de lavadoras Totalmente Gratis";
+      document.getElementById("downtitle").innerHTML = "Manual de reparación de lavadoras";
+      document.getElementById("downtype").innerHTML = "PDF";
+      document.getElementById("downloads").innerHTML = "6978";
+      document.getElementById("downsize").innerHTML = "(98 MB)";
+      document.getElementById("LINK").setAttribute("href", "https://drive.google.com/file/d/1EvjzoTooiYtMt3r9JqBDUEqN8TlPR50j");
+
+      function getLink() {
+        sessionStorage.clear(setTimeout(1000));
+        document.getElementById("contador").style.display = "inline";
+        document.getElementById("downboton").style.display = "none";
+        let time_out = 1000;
+        let counter_id;
+        let seconds = 15;
+        counter_id = setInterval(update_counter_link, time_out);
+        function update_counter_link() {
+          document.getElementById('link_button').innerHTML = (--seconds);
+          if (!seconds) {
+            clearInterval(counter_id);
+            document.getElementById("contador").style.display = "none";
+            document.getElementById("linkDownload").style.display = "inline";
+          }
+        }
+       
+       } else if (sessionStorage.getItem("setPagina") === "?manual-de-reparacion-de-refrigeradores") {
+      document.getElementById("donwload-title").innerHTML = "Descarga Aqui El Manual de Reparación de Refrigeradores Totalmente Gratis";
+      document.getElementById("downtitle").innerHTML = "Manual de Reparación de Refrigeradores";
+      document.getElementById("downtype").innerHTML = "PDF";
+      document.getElementById("downloads").innerHTML = "7430";
+      document.getElementById("downsize").innerHTML = "(14 MB)";
+      document.getElementById("LINK").setAttribute("href", "https://drive.google.com/file/d/1EvjzoTooiYtMt3r9JqBDUEqN8TlPR50j");
+
+      function getLink() {
+        sessionStorage.clear(setTimeout(1000));
+        document.getElementById("contador").style.display = "inline";
+        document.getElementById("downboton").style.display = "none";
+        let time_out = 1000;
+        let counter_id;
+        let seconds = 15;
+        counter_id = setInterval(update_counter_link, time_out);
+        function update_counter_link() {
+          document.getElementById('link_button').innerHTML = (--seconds);
+          if (!seconds) {
+            clearInterval(counter_id);
+            document.getElementById("contador").style.display = "none";
+            document.getElementById("linkDownload").style.display = "inline";
+          }
+        }
+      }else{
         console.log("N/link");
       }
 
