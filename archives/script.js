175d6e48f75fcaaa9d0e8eff3d3b04acdb0c43fa
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2023-09-07 12:08:57 -0600

    1
    1

--- archives/script.js
+++ archives/script.js
@@ -53,6 +53,15 @@ if (urlActual.includes('?descargar-archivo-3')) {
   }
 }
 
+if (urlActual.includes('?descargar-archivo-4')) {
+  if (urlActual.includes('item-2')) {
+    agregarImagen('https://i.imgur.com/5qsXVLu.png');
+    agregarEnlace('https://www.infoproyectos.com/?mecanica-automotriz', '< Descargar >');
+  } else {
+    agregarImagen('https://i.imgur.com/0ZudrUt.png');
+    agregarEnlace('?descargar-archivo-3#item-2', '< Ver archivo >');
+  }
+}
 
 if (urlActual.includes('?archivo-1')) {
   var randomUrls = Math.floor(Math.random() * urls.length);
@@ -63,4 +72,7 @@ if (urlActual.includes('?archivo-1')) {
 } else if (urlActual.includes('?archivo-3')) {
   var randomUrls = Math.floor(Math.random() * urls.length);
   window.location.href = urls[randomUrls] + '?descargar-archivo-3';
+} else if (urlActual.includes('?archivo-4')) {
+  var randomUrls = Math.floor(Math.random() * urls.length);
+  window.location.href = urls[randomUrls] + '?descargar-archivo-4';
 }
