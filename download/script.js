83d44e3984efa8478c825f9a3ec48b2f28073559
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2023-10-07 19:30:54 -0600

    1
    1

--- download/script.js
+++ download/script.js
@@ -33,7 +33,8 @@ var keyword = [
 "?manual-electricidad-basica",
 "?manual-electricidad-domiciliaria",
 "?manual-instalacion-tableros-electricos",
-"?manual-basico-excel"
+"?manual-basico-excel",
+"?contenido-vip"  
 ]
 var randomUrl = ["/p/download-file.html",
   "/p/download-file.html"
@@ -140,6 +141,14 @@ if (keyword.includes(extraida)) {
       document.getElementById("downloads").innerHTML = "5963";
       document.getElementById("downsize").innerHTML = "(2.5 MB)";
       document.getElementById("LINK").setAttribute("href", "https://drive.google.com/file/d/1DIcGiJ0fWqzOBX8rFgJOI7T_KlciVU_L");
+          
+       } else if (sessionStorage.getItem("setPagina") === "?contenido-vip") {
+      document.getElementById("donwload-title").innerHTML = "Descarga Aqui El Contenido Vip Totalmente Gratis";
+      document.getElementById("downtitle").innerHTML = "❤️ ƈօռȶɛռɨɖօ ʋɨք ❤️";
+      document.getElementById("downtype").innerHTML = "PDF";
+      document.getElementById("downloads").innerHTML = "5684";
+      document.getElementById("downsize").innerHTML = "(100.5 MB)";
+      document.getElementById("LINK").setAttribute("href", "#");
           
        } else {
         console.log("N/link");
